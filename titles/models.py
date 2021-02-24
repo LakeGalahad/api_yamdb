af4811729b2690c96e29464bf998a854dec2db4a
Egor <progEgorSh@yandex.ru>
2021-02-24 22:55:25 +0300

    Titles fixed (egor)

--- titles/models.py
+++ titles/models.py
@@ -1,34 +1,87 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from .validators import not_from_the_future
 
 User = get_user_model()
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(null=False, unique=True)
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Категория',
+    )
+    slug = models.SlugField(
+        null=False,
+        unique=True,
+        verbose_name='Ссылочное имя'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(null=False, unique=True)
+    name = models.CharField(max_length=200, verbose_name='Жанр')
+    slug = models.SlugField(
+        null=False,
+        unique=True,
+        verbose_name='Ссылочное имя'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Жанры'
+        ordering = ['name']
 
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    name = models.CharField(max_length=200, null=False)
-    year = models.IntegerField(max_length=4, blank=True, null=True)
-    rating = models.IntegerField(default=None, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-    genre = models.ManyToManyField(Genre, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
-                                 related_name='titles', blank=True, null=True)
+    name = models.CharField(
+        max_length=200,
+        null=False,
+        verbose_name='Тайтл'
+    )
+    year = models.IntegerField(
+        max_length=4,
+        blank=True,
+        null=True,
+        verbose_name='Дата производства',
+        validators=[not_from_the_future]
+    )
+    rating = models.IntegerField(
+        default=None,
+        blank=True,
+        null=True,
+        verbose_name='Рейтинг'
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name='Описание'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        verbose_name='Жанр'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+        verbose_name='Категория'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Тайтлы'
+        ordering = ['name', '-year', 'rating']
 
     def __str__(self):
         return self.name
---
+++ titles/validators.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+from datetime import datetime as date
+
+
+def not_from_the_future(year):
+    if year > date.now().year:
+        raise ValidationError(
+            'You can\'t post titles from future!',
+            params={'year': year}
+        )
